4bed0871e2f889694aa64403ed550d2fc5f94914
Haru <rururu-1o@live.jp>
2023-08-20 23:44:44 +0900

    fix of reservations controller

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -22,16 +22,5 @@ class ReservationsController < ApplicationController
       flash.now[:alert] = "予約情報に不備があります"
       render "rooms/show", locals: {id: @reservation.room_id}
     end
-
-    # if @reservation.checkin.blank? || @reservation.checkout.blank? || @reservation.people.blank?
-    #   puts "blank"
-    #   redirect_to @reservation.room
-    # elsif @reservation.checkin < Date.today || @reservation.checkin > @reservation.checkout
-    #   puts "checkout"
-    #   redirect_to @reservation.room
-    # elsif !@reservation.people.is_a?(Integer) || @reservation.people == 0
-    #   puts "people"
-    #   redirect_to @reservation.room
-    # end
   end  
 end
